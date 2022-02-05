f1964ba281dc62189d8fb51537a8358dcbaa7bb3
eremenko.mihail <eremenko.mihail@gmail.com>
2022-02-05 13:08:31 +0300

    Counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,100 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyFirstApp());
+void main() => runApp(const CounterApp());
 
-class MyFirstApp extends StatefulWidget {
-  MyFirstApp({Key? key}) : super(key: key);
-  @override
-  State<StatefulWidget> createState() {
-    return _MyFirstAppState();
-  }
-}
-
-class _MyFirstAppState extends State<MyFirstApp> {
-  late bool _loading;
-  late double _progressValue;
-
-  @override
-  void initState() {
-    _loading = false;
-    _progressValue = 0.0;
-    super.initState();
-  }
+class CounterApp extends StatelessWidget {
+  const CounterApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'IndieFlower'),
       home: Scaffold(
         backgroundColor: Colors.grey,
         appBar: AppBar(
           title: const Text("App for learning"),
           centerTitle: true,
         ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: _loading
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      LinearProgressIndicator(
-                        value: _progressValue,
-                      ),
-                      Text(
-                        "${(_progressValue * 100).round()} %",
-                        style:
-                            const TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    ],
-                  )
-                : const Text(
-                    "Press button to download",
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: const Icon(Icons.cloud_download),
+        body: const Center(
+          child: Counter(),
         ),
       ),
     );
   }
+}
 
-  void _updateProgress() {
-    const oneSec = const Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer t) {
-      setState(() {
-        _progressValue += 0.2;
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
 
-        if (_progressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          t.cancel();
-          _progressValue = 0.0;
-        }
-      });
-    });
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  late int _count;
+
+  @override
+  void initState() {
+    _count = 50;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Tap "-" to decrement',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+            Container(
+              //color: Colors.grey,
+              padding: const EdgeInsets.all(5.0),
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.indeterminate_check_box_outlined),
+                    color: Colors.black54,
+                    onPressed: () {
+                      setState(() {
+                        _count -= 1;
+                      });
+                    },
+                  ),
+                  Text(
+                    "$_count",
+                    style: const TextStyle(fontSize: 20, color: Colors.black54),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add_box_outlined),
+                    color: Colors.black54,
+                    onPressed: () {
+                      setState(() {
+                        _count += 1;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Tap "+" to increment',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
